d93831f10762696bd8138615244e9e78114069a2
ohtjqkd <ohtjqkd@gmail.com>
2021-05-17 15:31:56 +0900

    아직 완료하지 못함

---
+++ day5/venv/my_new_assistant/bicsubi_extra/app.py
@@ -0,0 +1,14 @@
+# import json
+# from urllib.request import urlopen
+# def get_location(ip):
+#     request
+
+
+# import requests
+# import geocoder
+# g = geocoder.ip('me')
+# print(g.latlng)
+# weather_api_key = '5e9db334d9b3a2ff5ddacf80f874ee3b'
+
+# api_url = f'https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={API key}'
+# requests.get()
